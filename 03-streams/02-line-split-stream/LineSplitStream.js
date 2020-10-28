3b7ffe986759a092f2c33b71e8ecbfbada96c6b8
writer_2159 <writer_2159@mail.ru>
2020-10-28 19:57:02 +0500

    сделан вариант обработки стрима с дележкой текста по строкам

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -2,15 +2,40 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
-  constructor(options) {
-    super(options);
-  }
+    constructor(options) {
+        super(options);
 
-  _transform(chunk, encoding, callback) {
-  }
+        this.encoding = options.encoding || 'utf8';
+        this.output = ''
+    }
 
-  _flush(callback) {
-  }
+
+    _transform(chunk, encoding, callback) {
+
+        const splitString = chunk.toString(this.encoding).split(os.EOL);  //разбиваем чанк по переносам
+
+        const lastKeyChunk = splitString.length - 1; //узнаем ключ последнего элемента чанка
+
+        splitString.forEach((value, i) => {
+
+            this.output += value; //складываем с дредыдущим остатком
+
+            if (i === lastKeyChunk) { //если это последний элемент, то не шлем его, а приплюсуем к следующему
+                return;
+            }
+
+            this.push(this.output); //отправляем собраный чанк
+            this.output = ''; //очищаем остаток
+        });
+
+        callback(null,chunk);
+    }
+
+    _flush(callback) {
+        //callback(null, this.output); //шлем остаток
+    }
+
+
 }
 
 module.exports = LineSplitStream;

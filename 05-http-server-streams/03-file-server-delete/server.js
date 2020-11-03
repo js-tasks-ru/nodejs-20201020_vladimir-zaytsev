e87277403e5f46c1c0f59326acdc61180c0fe220
writer_2159 <writer_2159@mail.ru>
2020-11-03 23:51:40 +0500

    сделано удаление файла

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,23 +1,56 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+    const pathname = url.parse(req.url).pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+    const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (req.method) {
-    case 'DELETE':
+    switch (req.method) {
+        case 'DELETE':
 
-      break;
+            //передали пустой путь
+            if (pathname === '') {
+                res.statusCode = 400;
+                res.end('Empty filename');
+                return;
+            }
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+            //передали вложенность
+            if (
+                pathname.includes('/')
+            ) {
+                res.statusCode = 400;
+                res.end('Subfolders not supported');
+                return;
+            }
+
+            fs.unlink(filepath, (error) => {
+                if (error === null) {
+                    res.end('file deleted');
+                    return;
+                }
+
+                if (error.code === 'ENOENT') {
+                    res.statusCode = 404;
+                    res.end('file not found');
+                    return;
+                }
+
+                res.statusCode = 500;
+                res.end('FAIL');
+            });
+
+            break;
+
+        default:
+            res.statusCode = 501;
+            res.end('Not implemented');
+    }
 });
 
 module.exports = server;

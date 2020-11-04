91cfbb387c86127c4274ee05b5d9112b1eed647b
writer_2159 <writer_2159@mail.ru>
2020-11-04 11:59:46 +0500

    06 long polling

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,50 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
+app.use(async (ctx, next) => {
+    try {
+        await next();
+    } catch (e) {
+        if(ctx.status){
+            ctx.throw(ctx.status,ctx.body);
+        }
+        ctx.throw(500,'FAIL');
+    }
 });
 
-router.post('/publish', async (ctx, next) => {
+//объект висящих подписок
+const storageSubscribes = new Set();
+
+router.get('/subscribe', async (ctx) => {
+
+    const message = await new Promise(resolve=>{
+        storageSubscribes.add(resolve);
+
+        ctx.req.on('close', function() {
+            storageSubscribes.delete(resolve);
+        });
+    });
+
+    ctx.status = 200;
+    ctx.body = message;
+
+});
+
+
+router.post('/publish', async (ctx) => {
+
+    const { message } = ctx.request.body;
+
+    if (!message){
+        ctx.throw(400, 'empty message');
+    }
+
+    for (const subscribeResolve of storageSubscribes) {
+        subscribeResolve(ctx.request.body.message);
+    }
+
+    ctx.statusCode = 201;
+    ctx.body = {message};
 });
 
 app.use(router.routes());

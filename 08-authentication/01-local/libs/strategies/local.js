0a37c72e6e7effcaa5a7f937cfff51aa9ad460b1
writer_2159 <writer_2159@mail.ru>
2020-11-25 18:24:55 +0500

    added try\catch 08-01

--- 08-authentication/01-local/libs/strategies/local.js
+++ 08-authentication/01-local/libs/strategies/local.js
@@ -4,21 +4,24 @@ const ModelUser = require('../../models/User');
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
     async function (email, password, done) {
+        try {
+            const user = await ModelUser.findOne({email});
 
-        const user = await ModelUser.findOne({email});
+            if (!user) {
+                done(null, false, 'Нет такого пользователя');
+                return;
+            }
 
-        if (!user) {
-            done(null, false, 'Нет такого пользователя');
-            return;
+            const passwordIsCorrect = await user.checkPassword(password);
+
+            if (!passwordIsCorrect) {
+                done(null, false, 'Неверный пароль');
+                return;
+            }
+
+            return done(null, user);
+        } catch (err) {
+            done(err);
         }
-
-        const passwordIsCorrect = await user.checkPassword(password);
-
-        if (!passwordIsCorrect) {
-            done(null, false, 'Неверный пароль');
-            return;
-        }
-
-        done(null, user);
     },
 );

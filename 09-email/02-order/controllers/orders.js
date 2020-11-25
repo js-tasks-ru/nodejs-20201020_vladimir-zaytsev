a4c435edaf2dc21eeab7cc8dde0939b0eb3b8a6e
writer_2159 <writer_2159@mail.ru>
2020-11-25 18:36:49 +0500

    09-02

--- 09-email/02-order/controllers/orders.js
+++ 09-email/02-order/controllers/orders.js
@@ -1,8 +1,28 @@
-const Order = require('../models/Order');
+const ModelOrder = require('../models/Order');
 const sendMail = require('../libs/sendMail');
+const mapOrder = require('../mappers/order');
 
-module.exports.checkout = async function checkout(ctx, next) {
+module.exports.checkout = async function checkout(ctx) {
+    const {product, phone, address} = ctx.request.body;
+    const {user} = ctx;
+
+    const order = await ModelOrder.create({user, product, phone, address});
+
+    await sendMail({
+        template: 'order-confirmation',
+        locals: {id: order.id, product},
+        to: user.email,
+        subject: 'Подтверждение заказа'
+    });
+
+    ctx.body = {order: order.id};
 };
 
-module.exports.getOrdersList = async function ordersList(ctx, next) {
+module.exports.getOrdersList = async function ordersList(ctx) {
+    const orders = await
+        ModelOrder
+            .find({user: ctx.user})
+            .then(orders => orders.map(order => mapOrder(order.populate('product'))));
+
+    ctx.body = {orders};
 };
